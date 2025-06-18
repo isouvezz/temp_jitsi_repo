d30e08361e4a3c7196225885ca317a3e4fec862e
장주명 <hoi@likelion.net>
2025-06-18 11:27:23 +0900

    💄 UPDATE: native에서 나가기 버튼 나오지 않던것 수정

--- react/features/toolbox/components/native/HangupContainerButtons.tsx
+++ react/features/toolbox/components/native/HangupContainerButtons.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-import { IReduxState } from '../../../app/types';
-import { IProps as AbstractButtonProps } from '../../../base/toolbox/components/AbstractButton';
-import HangupButton from '../HangupButton';
+import { IReduxState } from "../../../app/types";
+import { IProps as AbstractButtonProps } from "../../../base/toolbox/components/AbstractButton";
+import HangupButton from "../HangupButton";
 
-import HangupMenuButton from './HangupMenuButton';
+import HangupMenuButton from "./HangupMenuButton";
 
 const HangupContainerButtons = (props: AbstractButtonProps) => {
-    const { conference } = useSelector((state: IReduxState) => state['features/base/conference']);
+    const { conference } = useSelector((state: IReduxState) => state["features/base/conference"]);
     const endConferenceSupported = conference?.isEndConferenceSupported();
 
-    return endConferenceSupported
-
+    return endConferenceSupported ? (
         // @ts-ignore
-        ? <HangupMenuButton { ...props } />
-        : <HangupButton { ...props } />;
+        <HangupMenuButton {...props} />
+    ) : (
+        // @ts-ignore
+        <HangupMenuButton {...props} />
+    );
 };
 
 export default HangupContainerButtons;
